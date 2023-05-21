e29b71abd300b5a01279ef1be9a9e48105e10634
valenitnascattini <valens00@hotmail.com>
2023-05-20 23:12:22 -0300

    first commit

---
+++ scrum.js
@@ -0,0 +1,4 @@
+//backlog: requisitos del proyecto - priorizadas por ROI (retorno de inversion) - gestionada por product owner (clientes)
+//sprint backlog: requisitos dte sprint actual - gestionada por equipo de desarrollo
+//sprint: periodo en que se lleva a cabo el trabajo - duracion definida por el equipo
+//tablero de tareas - TRELLO
